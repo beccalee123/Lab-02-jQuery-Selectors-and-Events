78dc3aea672494062ea6ece3763421e57b5fa915
andrewquamme <andrewquamme@gmail.com>
2018-11-14 11:01:30 -0800

    renders to DOM

--- js/app.js
+++ js/app.js
@@ -10,12 +10,31 @@ function Horns(horn) {
 
 Horns.allHorns = [];
 
+Horns.prototype.render = function() {
+  $('main').append('<div class="clone"></div>');
+  let hornClone = $('div[class="clone"]'); //change dis?
+
+  let hornHTML = $('#photo-template').html();
+
+  hornClone.html(hornHTML);
+
+  hornClone.find('h2').text(this.title);
+  hornClone.find('img').attr('src', this.image_url);
+  hornClone.find('p').text(this.description);
+  hornClone.removeClass('clone');
+  hornClone.attr('class', this.title);
+};
+
 Horns.readJson = () => {
   $.get('data/page-1.json', 'json').then(data => {
     data.forEach(obj => {
       Horns.allHorns.push(new Horns(obj));
     });
-  });
+  }).then(Horns.loadHorns);
 };
+
+Horns.loadHorns = () => {
+  Horns.allHorns.forEach( horn => horn.render() );
+}
 
 $( () => Horns.readJson() );
